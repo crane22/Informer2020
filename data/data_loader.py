493b2c4ff6e9b27ea80e9254ae001fd1c39f5e81
Lily Crane <82160312+crane22@users.noreply.github.com>
2024-01-08 10:16:28 +0800

    Update data_loader.py

--- data/data_loader.py
+++ data/data_loader.py
@@ -215,62 +215,113 @@ class Dataset_Custom(Dataset):
         self.data_path = data_path
         self.__read_data__()
 
+    # def __read_data__(self):
+    #     self.scaler = StandardScaler()
+    #     df_raw = pd.read_csv(os.path.join(self.root_path,
+    #                                       self.data_path))
+    #     '''
+    #     df_raw.columns: ['date', ...(other features), target feature]
+    #     '''
+    #     # cols = list(df_raw.columns); 
+    #     if self.cols:
+    #         cols=self.cols.copy()
+    #         cols.remove(self.target)
+    #     else:
+    #         cols = list(df_raw.columns)
+    #         # Before trying to remove the columns, check if they exist
+    #         if 'date' in df_raw.columns:
+    #             cols.remove('date')
+    #         if self.target in df_raw.columns:
+    #             cols.remove(self.target)
+
+    #     # Now proceed with your processing
+    #     df_raw = df_raw[['date'] + cols + [self.target]] if 'date' in df_raw.columns else df_raw[cols + [self.target]]
+
+    #     #     ; cols.remove(self.target); cols.remove('date')
+    #     # df_raw = df_raw[['date']+cols+[self.target]]
+
+    #     num_train = int(len(df_raw)*0.7)
+    #     num_test = int(len(df_raw)*0.2)
+    #     num_vali = len(df_raw) - num_train - num_test
+    #     border1s = [0, num_train-self.seq_len, len(df_raw)-num_test-self.seq_len]
+    #     border2s = [num_train, num_train+num_vali, len(df_raw)]
+    #     border1 = border1s[self.set_type]
+    #     border2 = border2s[self.set_type]
+        
+    #     if self.features=='M' or self.features=='MS':
+    #         cols_data = df_raw.columns[1:]
+    #         df_data = df_raw[cols_data]
+    #     elif self.features=='S':
+    #         df_data = df_raw[[self.target]]
+
+    #     if self.scale:
+    #         train_data = df_data[border1s[0]:border2s[0]]
+    #         self.scaler.fit(train_data.values)
+    #         data = self.scaler.transform(df_data.values)
+    #     else:
+    #         data = df_data.values
+            
+    #     df_stamp = df_raw[['date']][border1:border2]
+    #     df_stamp['date'] = pd.to_datetime(df_stamp.date)
+    #     data_stamp = time_features(df_stamp, timeenc=self.timeenc, freq=self.freq)
+
+    #     self.data_x = data[border1:border2]
+    #     if self.inverse:
+    #         self.data_y = df_data.values[border1:border2]
+    #     else:
+    #         self.data_y = data[border1:border2]
+    #     self.data_stamp = data_stamp
+
     def __read_data__(self):
         self.scaler = StandardScaler()
-        df_raw = pd.read_csv(os.path.join(self.root_path,
-                                          self.data_path))
-        '''
-        df_raw.columns: ['date', ...(other features), target feature]
-        '''
-        # cols = list(df_raw.columns); 
+        df_raw = pd.read_csv(os.path.join(self.root_path, self.data_path))
+    
         if self.cols:
-            cols=self.cols.copy()
+            cols = self.cols.copy()
             cols.remove(self.target)
         else:
             cols = list(df_raw.columns)
-            # Before trying to remove the columns, check if they exist
-            if 'date' in df_raw.columns:
-                cols.remove('date')
             if self.target in df_raw.columns:
                 cols.remove(self.target)
-
-        # Now proceed with your processing
-        df_raw = df_raw[['date'] + cols + [self.target]] if 'date' in df_raw.columns else df_raw[cols + [self.target]]
-
-        #     ; cols.remove(self.target); cols.remove('date')
-        # df_raw = df_raw[['date']+cols+[self.target]]
-
-        num_train = int(len(df_raw)*0.7)
-        num_test = int(len(df_raw)*0.2)
+    
+        df_data = df_raw[cols]
+    
+        num_train = int(len(df_raw) * 0.7)
+        num_test = int(len(df_raw) * 0.2)
         num_vali = len(df_raw) - num_train - num_test
-        border1s = [0, num_train-self.seq_len, len(df_raw)-num_test-self.seq_len]
-        border2s = [num_train, num_train+num_vali, len(df_raw)]
+        border1s = [0, num_train - self.seq_len, len(df_raw) - num_test - self.seq_len]
+        border2s = [num_train, num_train + num_vali, len(df_raw)]
         border1 = border1s[self.set_type]
         border2 = border2s[self.set_type]
-        
-        if self.features=='M' or self.features=='MS':
-            cols_data = df_raw.columns[1:]
-            df_data = df_raw[cols_data]
-        elif self.features=='S':
-            df_data = df_raw[[self.target]]
-
+    
+        if self.features in ['M', 'MS']:
+            cols_data = df_data.columns
+        elif self.features == 'S':
+            cols_data = [self.target]
+    
         if self.scale:
             train_data = df_data[border1s[0]:border2s[0]]
             self.scaler.fit(train_data.values)
             data = self.scaler.transform(df_data.values)
         else:
             data = df_data.values
-            
-        df_stamp = df_raw[['date']][border1:border2]
-        df_stamp['date'] = pd.to_datetime(df_stamp.date)
-        data_stamp = time_features(df_stamp, timeenc=self.timeenc, freq=self.freq)
-
+    
+        # Handling datasets without 'date' column
+        if 'date' in df_raw.columns:
+            df_stamp = df_raw[['date']][border1:border2]
+            df_stamp['date'] = pd.to_datetime(df_stamp.date)
+            data_stamp = time_features(df_stamp, timeenc=self.timeenc, freq=self.freq)
+        else:
+            # If there's no 'date' column, handle accordingly (e.g., use default indices as time stamps)
+            data_stamp = np.arange(border1, border2).reshape(-1, 1)
+    
         self.data_x = data[border1:border2]
         if self.inverse:
             self.data_y = df_data.values[border1:border2]
         else:
             self.data_y = data[border1:border2]
         self.data_stamp = data_stamp
+
     
     def __getitem__(self, index):
         s_begin = index
